98b6297a58c9176a70547ed1201e2505b4b20807
m-taylor.13 <m-taylor.13@onu.edu>
2025-10-30 12:44:56 -0400

    completed hasAdjacentDuplicates

--- Project26/FileName.cpp
+++ Project26/FileName.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 bool isSortedIncreasing(int array[]);
 bool isSortedDecreasing(int array[]);
+bool hasAdjecentDuplicates(int array[]);
 
 int main(void) {
 
 	int nums[] = { 1,2,3 };
-	int nums2[] = { 3,2,1 };
+	int nums2[] = { 3,2,3 };
 
-	cout << isSortedIncreasing(nums) << endl;
-	cout << isSortedDecreasing(nums) << endl;
-	cout << isSortedDecreasing(nums2) << endl;
+	cout << hasAdjecentDuplicates(nums2) << endl;
 
 }
 
@@ -41,4 +40,17 @@ bool isSortedDecreasing(int array[]) {
 	}
 
 	return isSorted;
+}
+
+bool hasAdjecentDuplicates(int array[]) {
+
+	bool hasDupes = false;
+
+	for (int i = 1; i < (sizeof(array) / sizeof(array[0])); i++) {
+
+		if (array[i] == array[i - 1]) {
+			hasDupes = true;
+		}
+	}
+	return hasDupes;
 }
