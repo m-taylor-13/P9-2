8133b450077e81bae83365e3f1df9d4aede94044
m-taylor.13 <m-taylor.13@onu.edu>
2025-10-30 12:41:48 -0400

    completed isSortedDecreasing

--- Project26/FileName.cpp
+++ Project26/FileName.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 bool isSortedIncreasing(int array[]);
-
+bool isSortedDecreasing(int array[]);
 
 int main(void) {
 
 	int nums[] = { 1,2,3 };
+	int nums2[] = { 3,2,1 };
 
 	cout << isSortedIncreasing(nums) << endl;
+	cout << isSortedDecreasing(nums) << endl;
+	cout << isSortedDecreasing(nums2) << endl;
 
 }
 
@@ -19,6 +22,20 @@ bool isSortedIncreasing(int array[]) {
 	for (int i = 1; i < (sizeof(array) / sizeof(array[0])); i++) {
 
 		if (array[i] < array[i - 1]) {
+			isSorted = false;
+		}
+	}
+
+	return isSorted;
+}
+
+bool isSortedDecreasing(int array[]) {
+
+	bool isSorted = true;
+
+	for (int i = 1; i < (sizeof(array) / sizeof(array[0])); i++) {
+
+		if (array[i] > array[i - 1]) {
 			isSorted = false;
 		}
 	}
