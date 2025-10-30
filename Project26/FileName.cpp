84bd13d5485df7048bbfe94e2dad9db1458bf4b1
m-taylor.13 <m-taylor.13@onu.edu>
2025-10-30 13:08:10 -0400

    completed hasDuplicates

--- Project26/FileName.cpp
+++ Project26/FileName.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-bool isSortedIncreasing(int array[]);
-bool isSortedDecreasing(int array[]);
-bool hasAdjecentDuplicates(int array[]);
+bool isSortedIncreasing(int array[], int size);
+bool isSortedDecreasing(int array[], int size);
+bool hasAdjecentDuplicates(int array[], int size);
+bool hasDuplicates(int array[], int size);
 
 int main(void) {
 
-	int nums[] = { 1,2,3 };
-	int nums2[] = { 3,2,3 };
-
-	cout << hasAdjecentDuplicates(nums2) << endl;
+	int nums[] = { 1,2,2,4,5,6};
+	
+	cout << hasDuplicates(nums, 6) << endl;
 
 }
 
-bool isSortedIncreasing(int array[]) {
+bool isSortedIncreasing(int array[], int size) {
 
 	bool isSorted = true;
 
-	for (int i = 1; i < (sizeof(array) / sizeof(array[0])); i++) {
+	for (int i = 1; i < size; i++) {
 
 		if (array[i] < array[i - 1]) {
 			isSorted = false;
@@ -28,11 +28,11 @@ bool isSortedIncreasing(int array[]) {
 	return isSorted;
 }
 
-bool isSortedDecreasing(int array[]) {
+bool isSortedDecreasing(int array[], int size) {
 
 	bool isSorted = true;
 
-	for (int i = 1; i < (sizeof(array) / sizeof(array[0])); i++) {
+	for (int i = 1; i < size; i++) {
 
 		if (array[i] > array[i - 1]) {
 			isSorted = false;
@@ -42,15 +42,34 @@ bool isSortedDecreasing(int array[]) {
 	return isSorted;
 }
 
-bool hasAdjecentDuplicates(int array[]) {
+bool hasAdjecentDuplicates(int array[], int size) {
 
 	bool hasDupes = false;
 
-	for (int i = 1; i < (sizeof(array) / sizeof(array[0])); i++) {
+	for (int i = 1; i < size; i++) {
 
 		if (array[i] == array[i - 1]) {
 			hasDupes = true;
 		}
 	}
+	return hasDupes;
+}
+
+bool hasDuplicates(int array[], int size) {
+
+	bool hasDupes = false;
+	
+	for (int i = 0; i < size; i++) {
+
+		for (int j = 0; j < size; j++) {
+
+			if (i != j) { //ensures it doesn't count itself as a duplicate
+				if (array[i] == array[j]) {
+					hasDupes = true;
+				}
+			}
+		}
+	}
+
 	return hasDupes;
 }
